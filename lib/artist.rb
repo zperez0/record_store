bec050ed779c860c2862678ed74462299eae8e60
Yazmin Perez <zoeperez333@gmail.com>
2022-05-11 18:38:24 -0700

    add all(), save(), ==(), clear()

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,2 +1,33 @@
 class Artist
+  attr_reader :id
+  attr_accessor :name
+
+  def initalize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def self.all
+    returned_artists = DB.exec("SELECT * FROM artists;")
+    artists = []
+    returned_artists.each() do |artist|
+      name = artist.fetch("name")
+      id = artist.fetch("id").to_i
+      artists.push(Artist.new({:name => name, :id => id}))
+    end
+    artists
+  end
+
+  def save
+    result = DB.exec("INSERT INTO artists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch('id').to_i
+  end
+
+  def ==(artist_to_compare)
+    self.name() == artist_to_compare.name()
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM artists *;")
+  end
 end
