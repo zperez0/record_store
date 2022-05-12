4729982da082929b84213ab48a7f631ff9ea96bc
Yazmin Perez <zoeperez333@gmail.com>
2022-05-11 19:56:48 -0700

    update methods

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
   attr_reader :id
   attr_accessor :name
 
-  def initalize(attributes)
+  def initialize(attributes)
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
@@ -18,16 +18,57 @@ class Artist
     artists
   end
 
-  def save
-    result = DB.exec("INSERT INTO artists (name) VALUES ('#{@name}') RETURNING id;")
-    @id = result.first().fetch('id').to_i
+  def update(attributes)
+    if (attributes.has_key?(:name)) && (attributes.fetch(:name) != nil)
+      @name = attributes.fetch(:name)
+      DB.exec("UPDATE artists SET name = '#{@name}' WHERE id = #{@id};")
+    elsif (attributes.has_key?(:album_name)) && (attributes.fetch(:album_name) != nil)
+      album_name = attributes.fetch(:album_name)
+      album = DB.exec("SELECT * FROM albums WHERE lower(name)='#{album_name.downcase}';").first
+      if album != nil
+        DB.exec("INSERT INTO albums_artists (album_id, artist_id) VALUES (#{album['id'].to_i}, #{@id});")
+      end
+    end
+  end
+
+  def albums
+    albums = []
+    results = DB.exec("SELECT album_id FROM albums_artists WHERE artist_id = #{@id};")
+    results.each() do |result|
+      album_id = result.fetch("album_id").to_i()
+      album = DB.exec("SELECT * FROM albums WHERE id = #{album_id};")
+      name = album.first().fetch("name")
+      albums.push(Album.new({:name => name, :id => album_id}))
+    end
+    albums
   end
 
   def ==(artist_to_compare)
     self.name() == artist_to_compare.name()
   end
 
+  def save
+    results = DB.exec("INSERT INTO artists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = results.first().fetch("id").to_i
+  end
+
   def self.clear
     DB.exec("DELETE FROM artists *;")
+  end
+
+  def delete
+    DB.exec("DELETE FROM albums_artists WHERE artist_id = #{@id};")
+    DB.exec("DELETE FROM artists WHERE id = #{@id};")
+  end
+
+  def self.find(id)
+    artist = DB.exec("SELECT * FROM artists WHERE id = #{id};").first
+    if artist
+    name = artist.fetch("name")
+    id = artist.fetch("id").to_i
+    Artist.new({:name => name, :id => id})
+    else 
+      nil
+    end
   end
 end
